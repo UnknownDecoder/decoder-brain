eae528998f1dcd94c518703c5289f18119765827
Unknown Decoder <155003129+UnknownDecoder@users.noreply.github.com>
2025-07-10 08:30:40 +0530

    Create server.py

---
+++ server.py
@@ -0,0 +1,19 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+from DecoderBot import ChatBot
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/chat')
+def chat():
+    msg = request.args.get('msg')
+    return jsonify({"reply": f"You said: {msg}"})
+
+bot = ChatBot(name="Zen8", threshold=0.5)
+
+def load_data():
+	with open("brain.json", "r") as brain:
+		return json.load(brain)
+
+bot.train(load_data())
